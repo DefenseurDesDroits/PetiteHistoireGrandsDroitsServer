ec62646b0c352ab55f6c1e43d819092f01c66d1a
Yorick <yorick@ctrlaltdev.xyz>
2018-07-02 14:23:37 -0700

    server

--- index.js
+++ index.js
@@ -5,13 +5,14 @@ const nodemailer = require ('nodemailer')
 
 let app = express()
 let transporter = nodemailer.createTransport({sendmail: true})
+// let transporter = nodemailer.createTransport({streamTransport: true})
 
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+  next()
 })
 
 app.get('/', function(req, res){
@@ -19,42 +20,63 @@ app.get('/', function(req, res){
 })
 
 app.post('/', function(req, res){
-  if (writeToFile(req.body) && sendToMail(req.body)) {
-    if (writeToFile(req.body)) {
-      res.writeHead(200, {'Content-Type': 'text/plain'})
-      res.end('thanks')
+  writeToFile(req.body).then(() => {
+      console.log('Entry added.')
+      sendToMail(req.body).then(() => {
+        console.log('Mail sent.')
+        res.send('thanks')
+      })
     }
-  }
+  )
 })
 
 
 function writeToFile(body) {
-  let line = `"${body.firstname}", "${body.lastname}", "${body.email}", "${body.story}"\n`
-  fs.lstat('concours.csv', (err) => {
-    if (err) {
-      fs.appendFile('concours.csv', 'prenom, nom, email, histoire\n', (err) => {
-        fs.appendFile('concours.csv', line, (err) => {
-          return err ? false : true
+  return new Promise((resolve, reject) => {
+    let line = `"${body.firstname}", "${body.lastname}", "${body.email}", "${body.story}"\n`
+    fs.lstat('concours.csv', (err) => {
+      if (err) {
+        fs.appendFile('concours.csv', 'prenom, nom, email, histoire\n', (err) => {
+          fs.appendFile('concours.csv', line, (err) => {
+            if (err) {
+              console.warn(err)
+              reject(err)
+            } else {
+              resolve()
+            }
+          })
         })
-      })
-    } else {
-      fs.appendFile('concours.csv', line, (err) => {
-        return err ? false : true
-      })
-    }
+      } else {
+        fs.appendFile('concours.csv', line, (err) => {
+          if (err) {
+            console.warn(err)
+            reject(err)
+          } else {
+            resolve()
+          }
+        })
+      }
+    })
   })
 }
 
 function sendToMail(body) {
-  let content = `Prenom : ${body.firstname}\nNom : ${body.lastname}\nEmail : ${body.email}\n\nHistoire :\n${body.story}`
+  return new Promise((resolve, reject) => {
+    let content = `Prenom : ${body.firstname}\nNom : ${body.lastname}\nEmail : ${body.email}\n\nHistoire :\n${body.story}`
 
-  transporter.sendMail({
-    from: `${body.firstname} ${body.lastname} <${body.email}>`,
-    to: 'yorick@ctrlaltdev.xyz',
-    subject: '[Concours] Petite histoire des grands droits',
-    text: content
-  }, (err, info) => {
-    return err ? false : true
+    transporter.sendMail({
+      from: `${body.firstname} ${body.lastname} <${body.email}>`,
+      to: 'yorick@ctrlaltdev.xyz',
+      subject: '[Concours] Petite histoire des grands droits',
+      text: content
+    }, (err, info) => {
+      if (err) {
+        console.warn(err)
+        reject(err)
+      } else {
+        resolve()
+      }
+    })
   })
 }
 
